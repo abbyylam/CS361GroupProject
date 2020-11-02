474e5f1da473a4f66a204f05993bfe6c914913a9
Jack Edwards <jackedwards@protonmail.com>
2020-11-01 19:54:50 -0600

    Implement back-end recipe search using dummy data

---
+++ routes/searchEngine.js
@@ -0,0 +1,6 @@
+exports.search = function(req, res) {
+    let recipes = ['Pepperoni Pizza', 'Cheese Pizza', 'Mushroom Pizza', 'Supreme Pizza'];
+    let name = req.query.name?.toLowerCase();
+    let matches = recipes.filter(x => x.toLowerCase().includes(name));
+    res.send(matches);
+}
