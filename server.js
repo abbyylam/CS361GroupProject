212fa61e11fedef7a23eeaa90412636de7529f2f
peterst5 <peterst5@oregonstate.edu>
2020-11-16 21:42:19 -0600

    Node API has an endpoint to get a single ingredient from the DB

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const pool = mysql.createPool(dbconfig);
 /* Route Paths */
 const site = require('./routes/site');
 const searchEngine = require('./routes/searchEngine');
-const recipeListing = require('./routes/recipeListing');
+const recipeListing = require('./routes/recipeListing')(pool);
 const account = require('./routes/account')(pool);
 
 const app = express();
@@ -22,6 +22,7 @@ app.post('/api/account', account.create);
 
 app.get('/api/search', searchEngine.search);
 app.get('/api/recipe', recipeListing.recipe);
+app.get('/api/ingredient', recipeListing.ingredient);
 app.get('*', site.index);
 
 const port = process.env.PORT || 5000;
