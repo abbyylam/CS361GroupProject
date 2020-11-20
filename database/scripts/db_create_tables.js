61b2ba4f4ec62cc34a14ff53e57bdd6e81fe401a
Jack Edwards <jackedwards@protonmail.com>
2020-11-19 22:39:02 -0600

    Clean up db_create and db_drop scripts

--- database/scripts/db_create_tables.js
+++ database/scripts/db_create_tables.js
@@ -2,91 +2,129 @@ var mysql = require('mysql');
 var dbConfig = require('../db_config.js')
 var OnSqlError = require('../db_utils.js').OnSQLError;
 
-var con = mysql.createConnection(dbConfig);
+connect()
+    .then(createUserTable)
+    .then(createIngredientTable)
+    .then(createIssueTable)
+    .then(createIngredientIssueTable)
+    .then(createIngredientAlternativeTable)
+    .then(disconnect);
 
-process.stdout.write('Connecting to database... ');
-con.connect(function(err) {
-    if (err) OnSqlError(con, err);
-    process.stdout.write('Success\n');
+function connect() {
+    return new Promise(function(resolve, reject) {
+        process.stdout.write('Connecting to database... ');
 
-    process.stdout.write('Creating \'user\' table... \n');
-    var sql = 'CREATE TABLE IF NOT EXISTS user (' +
-        'Id int PRIMARY KEY NOT NULL AUTO_INCREMENT, ' +
-        'Email varchar(255) NOT NULL, ' +
-        'Password char(64) NOT NULL, ' + 
-        'SessionId char(64));';
-
-    con.query(sql, function(err, result) {
-        if (err) OnSqlError(con, err);
-        process.stdout.write('Success\n');
-
+        var con = mysql.createConnection(dbConfig);
+        con.connect(function(err) {
+            if (err) OnSqlError(con, err);
+            process.stdout.write('Success\n');
+            resolve(con);
+        });
     });
+}
 
-    process.stdout.write('Creating \'ingredient\' table... \n');
-    var sql = 'CREATE TABLE IF NOT EXISTS ingredient (' +
-        'Id int PRIMARY KEY NOT NULL AUTO_INCREMENT, ' +
-        'Name varchar(255) NOT NULL, ' +
-        'HasIssue boolean NOT NULL);';
+function createUserTable(con) {
+    return new Promise(function(resolve, reject) {
+        process.stdout.write('Creating \'user\' table... ');
+        var sql = 'CREATE TABLE IF NOT EXISTS user (' +
+            'Id int PRIMARY KEY NOT NULL AUTO_INCREMENT, ' +
+            'Email varchar(255) NOT NULL, ' +
+            'Password char(64) NOT NULL, ' + 
+            'SessionId char(64));';
     
-    con.query(sql, function(err, result) {
-        if (err) OnSqlError(con, err);
-        process.stdout.write('Success\n');
-
+        con.query(sql, function(err, result) {
+            if (err) OnSqlError(con, err);
+            process.stdout.write('Success\n');
+            resolve(con);
+        });
     });
+}
 
-    process.stdout.write('Creating \'issue\' table... \n');
-    var sql = 'CREATE TABLE IF NOT EXISTS issue (' +
-        'Id int PRIMARY KEY NOT NULL AUTO_INCREMENT, ' +
-        'Name varchar(255) NOT NULL, ' +
-        'Description varchar(255) NOT NULL, ' +
-        'EvidenceUrl varchar(255) NOT NULL);';
-
-    con.query(sql, function(err, result) {
-        if (err) OnSqlError(con, err);
-        process.stdout.write('Success\n');
-
+function createIngredientTable(con) {
+    return new Promise(function(resolve, reject) {
+        process.stdout.write('Creating \'ingredient\' table... ');
+        var sql = 'CREATE TABLE IF NOT EXISTS ingredient (' +
+            'Id int PRIMARY KEY NOT NULL AUTO_INCREMENT, ' +
+            'Name varchar(255) NOT NULL, ' +
+            'HasIssue boolean NOT NULL);';
+        
+        con.query(sql, function(err, result) {
+            if (err) OnSqlError(con, err);
+            process.stdout.write('Success\n');
+            resolve(con);
+        });
     });
+}
 
-    process.stdout.write('Creating \'ingredientissue\' table... \n');
-    var sql = 'CREATE TABLE IF NOT EXISTS ingredientIssue (' +
-        'Id int PRIMARY KEY NOT NULL AUTO_INCREMENT, ' +
-        'IngredientId int NOT NULL, ' +
-        'IssueId int NOT NULL,' +
-        'CONSTRAINT `fk_ingredient_id`' +
-            'FOREIGN KEY (IngredientId) REFERENCES ingredient (id)' +
-            'ON DELETE CASCADE,' +
-        'CONSTRAINT `fk_issue_id`' + 
-            'FOREIGN KEY (IssueId) REFERENCES issue (id)' + 
-            'ON DELETE CASCADE);';
-
-    con.query(sql, function(err, result) {
-        if (err) OnSqlError(con, err);
-        process.stdout.write('Success\n');
-
+function createIssueTable(con) {
+    return new Promise(function(resolve, reject) {
+        process.stdout.write('Creating \'issue\' table... ');
+        var sql = 'CREATE TABLE IF NOT EXISTS issue (' +
+            'Id int PRIMARY KEY NOT NULL AUTO_INCREMENT, ' +
+            'Name varchar(255) NOT NULL, ' +
+            'Description varchar(255) NOT NULL, ' +
+            'EvidenceUrl varchar(255) NOT NULL);';
+    
+        con.query(sql, function(err, result) {
+            if (err) OnSqlError(con, err);
+            process.stdout.write('Success\n');
+            resolve(con);
+        });
     });
+}
 
-    process.stdout.write('Creating \'ingredientAlternative\' table... \n');
-    var sql = 'CREATE TABLE IF NOT EXISTS ingredientAlternative (' +
-        'Id int PRIMARY KEY NOT NULL AUTO_INCREMENT, ' +
-        'IngredientId int NOT NULL, ' +
-        'AltIngredientId int NOT NULL,' +
-        'CONSTRAINT `fk__issue_ingredient_id`' +
-            'FOREIGN KEY (IngredientId) REFERENCES ingredient (id)' +
-            'ON DELETE CASCADE,' +
-        'CONSTRAINT `fk_alt_ingredient_id`' + 
-            'FOREIGN KEY (AltIngredientId) REFERENCES ingredient (id)' + 
-            'ON DELETE CASCADE);';
-
-    con.query(sql, function(err, result) {
-        if (err) OnSqlError(con, err);
-        process.stdout.write('Success\n');
-
+function createIngredientIssueTable(con) {
+    return new Promise(function(resolve, reject) {
+        process.stdout.write('Creating \'ingredientissue\' table... ');
+        var sql = 'CREATE TABLE IF NOT EXISTS ingredientIssue (' +
+            'Id int PRIMARY KEY NOT NULL AUTO_INCREMENT, ' +
+            'IngredientId int NOT NULL, ' +
+            'IssueId int NOT NULL,' +
+            'CONSTRAINT `fk_ingredient_id`' +
+                'FOREIGN KEY (IngredientId) REFERENCES ingredient (id)' +
+                'ON DELETE CASCADE,' +
+            'CONSTRAINT `fk_issue_id`' + 
+                'FOREIGN KEY (IssueId) REFERENCES issue (id)' + 
+                'ON DELETE CASCADE);';
+    
+        con.query(sql, function(err, result) {
+            if (err) OnSqlError(con, err);
+            process.stdout.write('Success\n');
+            resolve(con);
+        });
     });
+}
 
-    process.stdout.write('Disconnecting... ');
-    con.end(function(err) {
-        if (err) OnSqlError(con, err);
-        process.stdout.write('Success\n');
+function createIngredientAlternativeTable(con) {
+    return new Promise(function(resolve, reject) {
+        process.stdout.write('Creating \'ingredientAlternative\' table... ');
+        var sql = 'CREATE TABLE IF NOT EXISTS ingredientAlternative (' +
+            'Id int PRIMARY KEY NOT NULL AUTO_INCREMENT, ' +
+            'IngredientId int NOT NULL, ' +
+            'AltIngredientId int NOT NULL,' +
+            'CONSTRAINT `fk__issue_ingredient_id`' +
+                'FOREIGN KEY (IngredientId) REFERENCES ingredient (id)' +
+                'ON DELETE CASCADE,' +
+            'CONSTRAINT `fk_alt_ingredient_id`' + 
+                'FOREIGN KEY (AltIngredientId) REFERENCES ingredient (id)' + 
+                'ON DELETE CASCADE);';
+    
+        con.query(sql, function(err, result) {
+            if (err) OnSqlError(con, err);
+            process.stdout.write('Success\n');
+            resolve(con);
+        });
     });
-});
+}
 
+function disconnect(con) {
+    return new Promise(function(resolve, reject) {
+        process.stdout.write('Disconnecting... ');
+
+        con.end(function(err) {
+            if (err) OnSqlError(con, err);
+            process.stdout.write('Success\n');
+            resolve();
+        });
+    });
+}
