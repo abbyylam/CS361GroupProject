94d86b40eee705b89727591282872a2d12d12b55
Jack Edwards <jackedwards@protonmail.com>
2020-11-24 23:05:31 -0600

    Hook up recipe search to the database

--- client/src/App/pages/Search.jsx
+++ client/src/App/pages/Search.jsx
@@ -1,6 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { withRouter, BrowserRouter } from 'react-router-dom';
-import { RecipeSearch } from '../requests/Api';
+import { Fragment } from 'react';
+import { Link, withRouter, BrowserRouter } from 'react-router-dom';
+import { FetchRecipe, RecipeSearch } from '../requests/Api';
+
+class SearchResultView {
+    id;
+    name;
+    ingredients;
+}
+
+var searchResultStyle = {
+    color: '#212529'
+};
+
+var searchButtonStyle = {
+    marginTop: '5px'
+}
 
 function Search({ history }) {
     const queryStrings = new URLSearchParams(window.location.search);
@@ -33,28 +48,82 @@ function Search({ history }) {
     const performSearch = (name) => {
         RecipeSearch(name)
         .then(res => res.json())
-        .then((result) => {
-            setSearchResults(result);
-        });
-    };
+        .then((apiResponse) => {
+            return apiResponse.success
+                ?   apiResponse.data.map(result => {
+                        var searchResultView = new SearchResultView();
+                        searchResultView.id = result.Id;
+                        return searchResultView;
+                    })
+                : [];
+        })
+        .then(hydrateSearchResultViewList)
+        .then(setSearchResults);
+    }
 
-    const createResultItem = (recipeName) => {
+    function hydrateSearchResultViewList(list) {
+        return new Promise(function(resolve, reject) {
+            var totalRecipes = list.length;
+            var recipesLoaded = 0;
+            list.forEach(searchResult => {
+                FetchRecipe(searchResult.id)
+                    .then(res => res.json())
+                    .then(fetchResult => {
+                        console.log(fetchResult);
+                        if (fetchResult.success) {
+                            searchResult.name = fetchResult.data.name;
+                            searchResult.ingredients = fetchResult.data.ingredients;
+                        } else {
+                            searchResult.name = null;
+                            searchResult.ingredients = null;
+                        }
+                        console.log(searchResult);
+                        recipesLoaded++;
+                        if (recipesLoaded === totalRecipes) {
+                            console.log(list);
+                            resolve(list);
+                        }
+                    })
+            })
+        });
+    }
+
+    const createNoResultItem = () => {
         return (
             <div className="row">
-                {recipeName}
+                No results
             </div>
         );
+    }
+
+    const createResultItem = (searchResultViewItem) => {
+        return (searchResultViewItem.name)
+            ? (<Link to={"/recipe/" + searchResultViewItem.id}>
+                    <div className="column" style={searchResultStyle}>
+                        <h5 className="text-left mb-3">{searchResultViewItem.name}</h5>
+                        <h6 className="text-left">Ingredients:</h6>
+                        <p className="text-left">{searchResultViewItem.ingredients.reduce(function(total, current) {
+                            return (total.length > 0)
+                                ? total + ', ' + current.Name
+                                : current.Name;
+                        }, '')}</p>
+                        <p className="text-left">Potential issues: {searchResultViewItem.ingredients.reduce(function(total, current) {
+                            return Number(total) + Number(current.HasIssue);
+                        }, 0)}</p>
+                    </div>
+                </Link>)
+            : <React.Fragment/>;
     }
 
     // Search menu
     const searchForm = (
         <form className="d-flex">
-            <div className="mr-sm-2">
+            <div className="mr-sm-2 text-left">
                 <input className="form-control mr-sm-2" type="text" placeholder="Search for a recipe" aria-label="Search" value={searchValue} onChange={updateSearchValue} />
                 <label><input name="showMine" type="checkbox"/> Show my recipes</label>
             </div>
             <div className="mr-sm-2">
-                <button className="btn btn-dark" type="submit" onClick={onSearchSubmit}>Search</button>
+                <button className="btn btn-dark" style={searchButtonStyle} type="submit" onClick={onSearchSubmit}>Search</button>
             </div>
         </form>
     );
@@ -63,7 +132,7 @@ function Search({ history }) {
     const resultList = (
         searchResults.length > 0
         ? searchResults.map(x => createResultItem(x))
-        : createResultItem('No results')
+        : createNoResultItem()
     );
 
     // Page
--- routes/searchEngine.js
+++ routes/searchEngine.js
@@ -1,8 +1,51 @@
-exports.search = function(req, res) {
-    let recipes = ['Pepperoni Pizza', 'Cheese Pizza', 'Mushroom Pizza', 'Supreme Pizza'];
-    let name = req.query.name
-        ? req.query.name.toLowerCase()
-        : undefined;
-    let matches = recipes.filter(x => x.toLowerCase().includes(name));
-    res.send(matches);
+module.exports = function(pool) {
+    var module = {};
+
+    module.search = function (req, res) {
+
+        var recipeName = req.query.name
+            ? req.query.name.toLowerCase()
+            : undefined;
+
+        if (!recipeName || recipeName.length === 0) {
+            return res.status(200).json({
+                'success': true,
+                'message': '',
+                'data': []
+            });
+        }
+
+        recipeSearch(recipeName)
+            .then((results) => res.status(200).json(
+                    {
+                        'success': true,
+                        'message': '',
+                        'data': results
+                    }), (error) => {
+                        console.log(error);
+                        return res.status(500).json(
+                            {
+                                'success': false,
+                                'message': ''
+                            }
+                        );
+                });
+
+        function recipeSearch(searchValue) {
+            return new Promise(function(resolve, reject) {
+                var sql = 'SELECT Id from recipe WHERE Name LIKE ?;';
+                var values = ['%' + searchValue + '%'];
+
+                pool.query(sql, values, function(err, result) {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(result);
+                });
+            });
+        }
+    }
+    return module;
 }
+
+return module;
